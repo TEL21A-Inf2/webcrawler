49bc690621fca2f32a60693e1a242a733c97a99c
Reiner Hüchting <dev@reinerhuechting.de>
2022-06-26 00:28:27 +0200

    Doppelt definierte Testdaten zusammengeführt.

---
+++ htmldocument/testdata/testdata.go
@@ -0,0 +1,12 @@
+package testdata
+
+import _ "embed"
+
+//go:embed simplepagenolinks.html
+var SimplePageNoLinks string
+
+//go:embed simplepageonelink.html
+var SimplePageOneLink string
+
+//go:embed simplepagetwolinks.html
+var SimplePageTwoLinks string
